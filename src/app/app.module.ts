753798a8d3ff55da0276e52cd982289cb16db63b
Juan Jose Payan <jjuanjopayand@gmail.com>
2021-07-28 17:44:22 -0500

    creado el boton de agregar facturas y de agregar items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AppComponent } from "./app.component";
 import { FacturaComponent } from "./factura/factura.component";
@@ -7,7 +9,7 @@ import { ClienteComponent } from "./cliente/cliente.component";
 
 @NgModule({
   declarations: [AppComponent, FacturaComponent, ClienteComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/cliente/cliente.component.ts
+++ src/app/cliente/cliente.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cliente.component.css']
 })
 export class ClienteComponent{
-
-  cliente = "Don amable"
+  nombre = "Taller 2"
 
 }
